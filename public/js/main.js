b8e094a9dfd0a86ffe4d5d7b38d2780ff81d66f8
biglidio <lucas-fj@hotmail.com>
2019-05-25 21:38:40 -0300

    Game Over com eventos

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
-$('.campo').on('input', function(){
+var campo = $('.campo');
+campo.on('input', function(){
 	var campo = $(this);
 	var texto = campo.val();
 
@@ -7,4 +8,25 @@ $('.campo').on('input', function(){
 
 	var qtdCaracteres = texto.length;
 	$('#caracteres').text(qtdCaracteres + " caracter" + ((qtdCaracteres == 1) ? "" : "es"));
+});
+
+var tempoRestante = 3;
+$('#segundos').text(tempoRestante + " segundo" + ((tempoRestante == 1) ? "" : "s"));
+
+campo.one('focus', function(){
+	var intervalo = setInterval(function(){
+		tempoRestante--;
+		$('#segundos').text(tempoRestante + " segundo" + ((tempoRestante == 1) ? "" : "s"));
+		
+		if(tempoRestante < 1) {
+			campo.attr('disabled', 1);
+			clearInterval(intervalo);
+			
+			var gameOver = $(document.createElement('div'));
+			gameOver.text('Game Over!')
+				.css('color', 'red')
+				.css('font-style', 'italic');
+			$(campo).after(gameOver);
+		}
+	}, 1000); 
 });
