1659b9d97f050ca8f7a09eadd25e170150156e21
biglidio <lucas-fj@hotmail.com>
2019-05-25 21:07:18 -0300

    Eventos do jQuery

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,10 @@
-var qtdPalavras = $('.frase').text().split(" ").length;
+$('.campo').on('input', function(){
+	var campo = $(this);
+	var texto = campo.val();
 
-$('#tamanho-frase').text(qtdPalavras + " palavra" + ((qtdPalavras == 1)?"":"s"));
+	var qtdPalavras = texto.split(/\S+/).length -1;
+	$('#palavras').text(qtdPalavras + " palavra" + ((qtdPalavras == 1) ? "" : "s"));
+
+	var qtdCaracteres = texto.length;
+	$('#caracteres').text(qtdCaracteres + " caracter" + ((qtdCaracteres == 1) ? "" : "es"));
+});
